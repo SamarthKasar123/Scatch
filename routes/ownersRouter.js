73e9dc1ae6d11d72b814aa50d84f71007d734c9f
SamarthKasar123 <samarthkasar9924@gmail.com>
2025-02-06 21:49:45 +0530

    User Routes Created

--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
+const ownerModel = require("../models/owners-model");
+
+if (process.env.NODE_ENV === "development") {
+    router.post("/create", function (req,res) {
+        res.send("hey its working");
+    });
+}
 
 router.get("/", function (req,res) {
     res.send("hey its working");
 });
+
 
 module.exports = router;
 
